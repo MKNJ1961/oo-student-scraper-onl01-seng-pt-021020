1d1347b0e535b66429849d51a309b6b5c73fa5b0
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-19 11:02:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,12 @@
 require 'open-uri'
-require 'nokogiri'
+# require 'nokogiri'
 require 'pry'
 
 class Scraper
 
-  attr_accessor :name, :location, :profile_url
-
-  @@all = []
-
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
-  end
 
+  end
 
   def self.scrape_profile_page(profile_url)
 
